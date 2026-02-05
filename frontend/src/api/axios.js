ea456c73d1571ed0418d940bfdeeb771ad129d5b
Deepan811 <deepanrajy@gmail.com>
2026-02-05 17:49:21 +0530

    new backend-link added and redirects fileadeed for routing in frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://iaxxin-mini-crm.onrender.com/api"
 });
 
 // Attach token automatically
